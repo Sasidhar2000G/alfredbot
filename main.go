541879575a03bf88180fceac50a8a06954261a0d
Sasidhar2000G <bleedingvibranium@gmail.com>
2022-10-23 13:04:13 +0530

    using a token file for security reasons

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-// APIKey for alfredbot from @botFather
-const token string = "5655859813:AAEm41cfxwd33OAMxOZgXHa9THBRu3ADm_E"
+// Error Codes
+const (
+	NO_ERROR int = iota
+	TOKEN_FILE_MISSING
+)
 
+// APIKey for alfredbot from @botFather
+var token string = func() string {
+	token, err := ioutil.ReadFile("token.txt")
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	log.Println(string(token))
+	return string(token)
+}()
+
+// right now the bot just echoes messages sent to it.
 func main() {
+
+	if len(token) == 0 {
+		log.Println("Token file not present.")
+		os.Exit(TOKEN_FILE_MISSING)
+	}
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = true // debug turned on for now for testing purposes.
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
